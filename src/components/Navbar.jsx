6e9fd35264125f04c54f0fece8f16a2d208809d8
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-01 17:26:38 +0100

    improved the responsiveness of the signup and login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ function Navbar(){
     return( 
       <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
          <div class="container-fluid"> 
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="/">
           <img src={logo} alt="imagelogo" />
-        </a>
+        </Link>
         
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -18,19 +18,6 @@ function Navbar(){
 
         <div className="collapse navbar-collapse align-self-middle p-3" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Courses
-                </a>
-
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-
-                </ul>
-          </li>
         
             <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#about">About us</a>
