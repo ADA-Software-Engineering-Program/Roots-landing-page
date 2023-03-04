cb1516a1b2a6f8dec7537a4ead364a2bda5c94be
Ifechukwu Max-Oti <60789147+ifechiglory@users.noreply.github.com>
2023-03-04 16:15:49 +0100

    Revert "Link login/sign up pages to dashboard"

--- src/pages/User/Dashboard.jsx
+++ src/pages/User/Dashboard.jsx
@@ -1,16 +1,13 @@
 import React from "react"; 
 import  "./Dashboard.css";
-import {Link} from 'react-router-dom';
 
 function Dashboard () {
     return (
         <div className="dashboard text-center">
             <h1 className="username mb-3 text-center mb-5 mt-5">Hello👋🏾, Welcome to Roots!</h1>
-            <Link to='/'>
-                <button className="startbtn ">
-                    Start your learning journey 
-                </button>
-            </Link>
+            <button className="startbtn ">
+                Start your learning journey 
+              </button>
         </div>
     )
 }
