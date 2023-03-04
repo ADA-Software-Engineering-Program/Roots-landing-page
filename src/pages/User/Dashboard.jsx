2bad1b68d080a4d222118c94c158469b48944586
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-03-04 14:22:45 +0100

    fixed dashboard button

--- src/pages/User/Dashboard.jsx
+++ src/pages/User/Dashboard.jsx
@@ -4,8 +4,8 @@ import  "./Dashboard.css";
 function Dashboard () {
     return (
         <div className="dashboard text-center">
-            <h1 className="username mb-3 text-center mb-4 mt-5">Hello👋🏾 , Welcome to Roots!</h1>
-            <button className="submit">
+            <h1 className="username mb-3 text-center mb-5 mt-5">Hello👋🏾, Welcome to Roots!</h1>
+            <button className="startbtn ">
                 Start your learning journey 
               </button>
         </div>
