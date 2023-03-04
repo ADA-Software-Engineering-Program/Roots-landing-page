f3cde6a62005918df3bdc1affe57ddc93b4b402c
Ifechukwu <ifechiglory@gmail.com>
2023-03-04 18:10:24 +0100

    bug fixes

--- src/pages/User/Dashboard.jsx
+++ src/pages/User/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard () {
         <div className="dashboard text-center">
             <h1 className="username mb-3 text-center mb-5 mt-5">Hello👋🏾, Welcome to Roots!</h1>
 
-                <a href="https://roots-green.vercel.app/" target='_blank'>
+                <a href="https://roots-green.vercel.app/">
             <button className="startbtn ">
                 Start your learning journey 
               </button></a>
